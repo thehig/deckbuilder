f9008a7d24b9093a54c72f89f09afad8728a75ae
Ben Murphy <ben@vonbismark.com>
2014-12-02 18:25:19 +0000

    Restyled the pregame deck selection view. Added some more bootstrap classes to pretty up some of the elements

--- client/game/game.js
+++ client/game/game.js
@@ -1,3 +1,96 @@
+
+
+
+// Player Status
+Template.game_pregame_player_status.helpers({
+    mine: function(){return this.playerId === Meteor.userId();},
+    username: function(){return Meteor.users.findOne({_id: this.playerId}).username; },
+    deckname: function(){
+        var deck = Decks.findOne({_id: this.deckId});
+        return deck ? deck.name : "Undecided";
+    }
+});
+
+Template.game_pregame_player_status.events({
+    'click .btn-action-ready': function(evt, template){
+        console.log(this);
+
+        var game = Session.get('currentGame');
+        //var player = game.players.findOne({playerId: Meteor.userId()});
+        game.players.forEach(function(player){
+            if(player.playerId === Meteor.userId()){
+                player.ready = !player.ready;
+                Meteor.call('updateGame', game);
+                return;
+            }
+        });
+    }
+});
+
+
+
+// Deck Selector
+Template.game_dc_dropdown.helpers({
+    decks: function(){ return Decks.find(); },
+    deckname: function() {
+        var deck = Decks.findOne({_id: this.deckId});
+        if(deck)
+            return deck.name;
+        return "Undecided";
+    }
+});
+
+Template.game_dc_dropdown.events({
+    'click .game-dc-dropdown-item': function(evt, template){
+        var clickedDeckName = evt.target.innerText;
+        var deck = Decks.findOne({name: clickedDeckName});
+        //console.log(deck._id);
+        //Meteor.call('chooseDeck', deck._id);
+
+        var game = Session.get('currentGame');
+        //var player = game.players.findOne({playerId: Meteor.userId()});
+        game.players.forEach(function(player){
+            if(player.playerId === Meteor.userId()){
+                player.deckId = deck._id;
+                Meteor.call('updateGame', game);
+                return;
+            }
+        });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //<!-- Game destination without a parameter -->
 Template.game.helpers({
     gamesInProgress: function(){ return Games.find()}, //{inProgress: true}
@@ -53,37 +146,7 @@ Template.game_pregame.events({
     }
 });
 
-// Player Status
-Template.game_player_status.helpers({
-    mine: function(){return this.playerId === Meteor.userId();},
-    username: function(){return Meteor.users.findOne({_id: this.playerId}).username; },
-    deckname: function(){
-        var deck = Decks.findOne({_id: this.deckId});
-        return deck ? deck.name : "Undecided";
-    }
-});
 
-Template.game_player_status.events({
-    'click button': function(evt, template){
-        console.log(this);
-
-        var game = Session.get('currentGame');
-        //var player = game.players.findOne({playerId: Meteor.userId()});
-        game.players.forEach(function(player){
-            if(player.playerId === Meteor.userId()){
-                player.ready = !player.ready;
-                Meteor.call('updateGame', game);
-                return;
-            }
-        });
-    }
-});
-
-// Deck Selector
-Template.game_deckselector_dropdown.helpers({
-    decks: function(){ return Decks.find(); }
-    //deckname: function() { return Decks.findOne({_id: this.deckId}).name; }
-});
 
 Template.game_deckselector_dropdown.events({
     'change': function(evt, template){
