09babd260d30527c0e6abd2ec02f6902647cc8eb
David Higgins <thehig@gmail.com>
2015-02-08 18:50:35 +0000

    Moved the Planechase loader into the server

--- collections.js
+++ collections.js
@@ -1,6 +1,7 @@
 Decks = new Meteor.Collection('decks');
 Cards = new Meteor.Collection('cards');
 Games = new Meteor.Collection('games');
+Planes = new Meteor.Collection('planes');
 
 if(Meteor.isServer) {
     //Server side publishing of Decks
@@ -13,6 +14,9 @@ if(Meteor.isServer) {
     Meteor.publish('games', function () {
         return Games.find();
     });
+    Meteor.publish('planes', function () {
+        return Planes.find();
+    });
     Meteor.publish('users', function () {
         return Meteor.users.find();
     });
@@ -24,6 +28,7 @@ if(Meteor.isClient){
     Meteor.subscribe('users');
     Meteor.subscribe('cards');
     Meteor.subscribe('games');
+    Meteor.subscribe('planes');
 
     // Set the automatic login prompts to request usernames only for brevity
     Accounts.ui.config({
--- server/Planechase.js
+++
@@ -1,7 +0,0 @@
-FS = Meteor.npmRequire('fs');
-
-var planes = process.env.PWD + "/public/images/planechase/";
-FS.readdir(planes, function (err, files){
-	console.log(files);
-})
-console.log("Planechase...")
