e4a7ad8ab5eef4bdc2398c42524da84bf1dd5672
David Higgins <thehig@gmail.com>
2015-02-08 19:27:14 +0000

    Planes displaying as checkboxes
    With jQuery to get the checked selection afterward

--- client/plane/plane.js
+++ client/plane/plane.js
@@ -4,3 +4,16 @@ Template.plane.helpers({
 		return Planes.find({});
 	}
 })
+
+Template.plane.events({
+	'click #startplanechase': function (evt, template){
+		evt.preventDefault();
+		var categories = [];
+		$('input[name=planechoice]:checked').each(function() {
+			categories.push($(this).val());
+		});
+
+
+		console.log("Ready " + categories);
+	}
+})
