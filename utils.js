16fe209cea0989c562ccf2134fd834ffeb434bf0
David Higgins <thehig@gmail.com>
2015-03-01 16:46:19 +0000

    Utils.js #29
    Heavy commenting of the utils file

--- utils.js
+++ utils.js
@@ -1,23 +1,47 @@
 utils = {
     lookup: {
+        /**
+         * Get a users Name by ID
+         * @param  {String} userId MongoDB ID for User
+         * @return {String}        Plaintext name for User
+         */
         username: function (userId) {
             return Meteor.users.findOne({_id: userId}).username;
         },
+        /**
+         * Gets a Deck by ID
+         * @param  {String} deckId MongoDB ID for Deck
+         * @return {Object}        Deck object from Database
+         */
         deck: function (deckId) {
             return Decks.findOne({_id: deckId});
         },
+        /**
+         * Gets a Deck Name by ID
+         * @param  {String} deckId MongoDB ID for Deck
+         * @return {String}        Deck name from Database or undefined if not found
+         */
         deckname: function (deckId) {
             var deck = utils.lookup.deck(deckId);
             return deck ? deck.name : undefined;
         }
     },
     game: {
-        //Provided a game object, return my players object or undefined
+        /**
+         * Look through a games players and determine if you are an active player
+         * @param  {Object} game Game object
+         * @return {Boolean}     True if you are a player in the game
+         */
         me: function (game) {
             return game ? _.findWhere(game.players, {playerId: Meteor.userId()}) : undefined;
         }
     },
     datastructures: {
+        /**
+         * Given a list of player IDs create a game for them
+         * @param  {[String]} playerIds Collection of MongoDB User IDs
+         * @return {Object}             JSON object representing a game
+         */
         creategame: function (playerIds) {
             return {
                 created: new Date(),
@@ -38,6 +62,11 @@ utils = {
                 }
             };
         },
+        /**
+         * Given a player ID, create a player Game Object
+         * @param  {String} playerId MongoDB Player ID
+         * @return {Object}          JSON object representing a single player in an ongoing game
+         */
         createplayer: function (playerId) {
             return {
                 playerId: playerId,
@@ -55,6 +84,11 @@ utils = {
                 }
             }
         },
+        /**
+         * Given a card, copy it and give it a unique ID for the database
+         * @param  {Object} card Original Card object with API data
+         * @return {Object}      JSON object representing the ID of the original card, and its state
+         */
         createbattlefieldcard: function (card) {
             return {
                 _id: card._id,
@@ -63,6 +97,11 @@ utils = {
                 tapped: false
             };
         },
+        /**
+         * Take in a message, sanatize it and return it
+         * @param  {String} chatmessage Users message to send
+         * @return {Object}             JSON object of the chat message and metadata
+         */
         createchatmessage: function(chatmessage){
             if(chatmessage.length > 140) chatmessage = chatmessage.substring(0, 140);
 
@@ -80,9 +119,9 @@ utils = {
 if(Meteor.isServer){
     utils.server = {
         /**
-         * Lookup a game, and player by gameId
-         * @param gameId
-         * @returns {{game: any, me: *}}
+         * Server side lookup of a game
+         * @param  {String} gameId MongoDB game ID
+         * @return {Object}        JSON object with the game and you in the game, if you are part of the game
          */
         lookup: function(gameId){
             var game = Games.findOne({_id: gameId});
@@ -92,15 +131,28 @@ if(Meteor.isServer){
             } : undefined;
         },
         /**
-         * Update a game object
-         * @param game
+         * Server side shortcut for upating a game
+         * @param  {Object} game        JSON object representing a game
+         * @return {WriteResult}        Status of the MongoDB operation
          */
         update: function(game){
             Games.update({_id: game._id}, game);
         },
+        /**
+         * Given a collection of cards, find the first one with the given card ID, owned by this player
+         * @param  {String} cardUid         MongoDB Card ID
+         * @param  {[Object]} collection    Array of Card Objects
+         * @return {Object}                 First card found, or null if not found
+         */
         findCard:function(cardUid, collection){
             return _.findWhere(collection, {_id: cardUid, owner: Meteor.userId()});
         },
+        /**
+         * Given a player find a given cards location
+         * @param  {String} cardUid MongoDB Card ID
+         * @param  {Object} player  JSON Player Object
+         * @return {Object}         An object with the Card Object, and the location where it was found, or undefined if not found
+         */
         findFromPlayer: function(cardUid, player){
             var card = utils.server.findCard(cardUid, player.hand);
             if(card) return {card: card, location: 'hand'};
@@ -116,40 +168,39 @@ if(Meteor.isServer){
 
             card = utils.server.findCard(cardUid, player.exile);
             if(card) return {card: card, location: 'exile'};
+
+            return undefined;
         },
         /**
-         * Lookup a card with the given cardId, splice it and return it
-         * @param cardUid
-         * @param collection
-         * @returns {T}
+         * Given a cardUid and a collection, remove the card from the collection and return it if owned by the invoking player
+         * @param  {String} cardUid         MongoDB ID for Card in Game (Not the original card ID)
+         * @param  {[Object]} collection    Array of Card Objects
+         * @return {Object}                 Card Object if found, or undefined
          */
         spliceCard:function(cardUid, collection){
             var card = _.findWhere(collection, {_id: cardUid, owner: Meteor.userId()});
             if(!card) return undefined;
             var spliced = collection.splice(collection.indexOf(card), 1)[0];
             return spliced ? spliced : undefined;
-        },
+        },        
         /**
-         * Lookup a card with the given cardId in the players hand, land and nonland, splice it and return it
-         * @param cardUid
-         * @param player
-         * @returns {*}
+         * Given a player, search their hand, battlefield, graveyard and exile and get the specified card
+         * @param  {String} cardUid MongoDB ID for Card in Game (Not the original card ID)
+         * @param  {Object} player  JSON Player Object
+         * @return {Object}         Card Object, or Undefined if not found
          */
         splicePlayer: function(cardUid, player){
-            var card = utils.server.spliceCard(cardUid, player.hand);
-            if(card) return card;
+            var lookup = utils.server.findFromPlayer(cardUid, player);
+            if(!lookup) return;
 
-            card = utils.server.spliceCard(cardUid, player.field.land);
-            if(card) return card;
-
-            card = utils.server.spliceCard(cardUid, player.field.nonland);
-            if(card) return card;
-
-            card = utils.server.spliceCard(cardUid, player.graveyard);
-            if(card) return card;
-
-            card = utils.server.spliceCard(cardUid, player.exile);
-            if(card) return card;
+            switch(lookup.location){
+                case 'hand':        return utils.server.spliceCard(cardUid, player.hand);
+                case 'land':        return utils.server.spliceCard(cardUid, player.field.land);
+                case 'nonland':     return utils.server.spliceCard(cardUid, player.field.nonland);
+                case 'graveyard':   return utils.server.spliceCard(cardUid, player.graveyard);
+                case 'exile':       return utils.server.spliceCard(cardUid, player.exile);
+                default:            return undefined;
+            }
         }
     };
 }
